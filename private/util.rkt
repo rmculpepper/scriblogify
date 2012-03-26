bee91b3179c1fbb688b8077ab69f4d5d2b1c42cd
Ryan Culpepper <ryanc@racket-lang.org>
2012-03-26 00:42:35 -0600

    improve profile fetching

--- private/util.rkt
+++ private/util.rkt
@@ -43,26 +43,39 @@ where profile = (cons name-symbol
   (define version (get-pref 'version))
   (unless (member version '(0 #f))
     (error 'scriblogify "unknown version: ~s" version))
+  (define profiles (or (get-pref 'profiles) null))
+  (define-values (get-blog get-album)
+    (match (dict-ref profiles profile)
+      [(list blog-info album-info)
+       (values (match blog-info
+                 [(list 'blogger blog-id blog-title)
+                  (lambda (oauth2)
+                    (let ([b-user (blogger #:oauth2 oauth2)])
+                      (or (send b-user find-child-by-id blog-id)
+                          (error 'scriblogify "could not find blog with id ~s" blog-id))))])
+               (match album-info
+                 [(list 'picasa album-id album-title)
+                  (lambda (oauth2)
+                    (let ([p-user (picasa #:oauth2 oauth2)])
+                      (or (send p-user find-child-by-id album-id)
+                          (error 'scriblogify "could not find album with id ~s" album-id))))]
+                 [#f #f]))]
+      [#f (error 'scriblogify "profile not found: ~s" profile)]))
   (define oauth2
     (match (get-pref 'auth)
       [(list 'oauth2-refresh-token refresh-token)
        (oauth2/refresh-token google-auth-server the-client refresh-token)]
-      [#f
-       (oauth2/request-auth-code/browser google-auth-server the-client (list blogger-scope picasa-scope))]
+      [#f #f]
       [_ (error 'scriblogify "unknown authorization")]))
-  (define profiles (or (get-pref 'profiles) null))
-  (match (dict-ref profiles profile)
-    [(list blog-info album-info)
-     (values oauth2
-             (match blog-info
-               [(list 'blogger blog-id blog-title)
-                (let ([b-user (blogger #:oauth2 oauth2)])
-                  (or (send b-user find-child-by-id blog-id)
-                      (error 'scriblogify "could not find blog with id ~s" blog-id)))])
-             (match album-info
-               [(list 'picasa album-id album-title)
-                (let ([p-user (picasa #:oauth2 oauth2)])
-                  (or (send p-user find-child-by-id album-id)
-                      (error 'scriblogify "could not find album with id ~s" album-id)))]
-               [#f #f]))]
-    [#f (error 'scriblogify "profile not found: ~s" profile)]))
+  (when oauth2 (send oauth2 validate!)) ;; set scopes
+  (let ([oauth2*
+         (cond [(and oauth2 (or (not get-album) (member picasa-scope (send oauth2 get-scopes))))
+                oauth2]
+               [else
+                (oauth2/request-auth-code/browser google-auth-server the-client
+                                                  (if get-album
+                                                      (list blogger-scope picasa-scope)
+                                                      (list blogger-scope)))])])
+    (values oauth2*
+            (get-blog oauth2*)
+            (and get-album (get-album oauth2*)))))
